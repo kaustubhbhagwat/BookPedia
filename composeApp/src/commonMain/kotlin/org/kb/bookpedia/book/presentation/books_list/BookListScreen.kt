1d033aa9c55ee1886d380a06ff80171a8da8f2e9
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-12-20 20:25:28 +0530

    implementing book list screen

--- composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/BookListScreen.kt
@@ -1,8 +1,39 @@
 package org.kb.bookpedia.book.presentation.books_list
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.kb.bookpedia.book.domain.Book
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun BookListScreen(){
+fun BookListScreenRoot(
+    viewModel: BookListViewModel = koinViewModel(),
+    modifier: Modifier = Modifier,
+    onBookClick: (Book) ->  Unit
+){
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    BookListScreen(
+        state = state,
+        onAction =  { action ->
+            when(action){
+                is BookListAction.OnBookClick -> onBookClick(action.book)
+                else -> {
+
+                }
+            }
+
+        }
+    )
+}
+
+@Composable
+fun BookListScreen(
+    state: BooksListState,
+    onAction: (BookListAction) -> Unit
+){
 
 }

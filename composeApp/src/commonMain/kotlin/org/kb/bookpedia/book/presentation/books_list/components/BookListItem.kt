e85a64356ee7791cbeee6a9482ed4e7252d9d969
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-02-04 20:20:25 +0530

    creating booklist item

--- composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/components/BookListItem.kt
@@ -1,9 +1,21 @@
 package org.kb.bookpedia.book.presentation.books_list.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
 import org.kb.bookpedia.book.domain.Book
 import org.kb.bookpedia.core.presentation.LightBlue
@@ -13,7 +25,7 @@ fun BookListItem(
     book: Book,
     onClick: () -> Unit,
     modifier: Modifier = Modifier
-){
+) {
 
     Surface(
         shape = RoundedCornerShape(32.dp),
@@ -21,6 +33,23 @@ fun BookListItem(
         onClick = onClick,
         color = LightBlue.copy(alpha = 0.2f)
     ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min)
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(100.dp),
+                contentAlignment = Alignment.Center
+            ){
+                var imageLoadResult by remember{
+                    mutableStateOf<Result<Painter>?>(null)
+                }
+
+            }
+        }
 
     }
 

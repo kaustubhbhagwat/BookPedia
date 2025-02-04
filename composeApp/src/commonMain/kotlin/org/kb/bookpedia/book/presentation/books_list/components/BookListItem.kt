95f6ce87c6aaf1f62af8593753d8d5952edd548f
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-02-04 20:15:47 +0530

    creating booklist item

--- composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/components/BookListItem.kt
@@ -1,9 +1,12 @@
 package org.kb.bookpedia.book.presentation.books_list.components
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.kb.bookpedia.book.domain.Book
+import org.kb.bookpedia.core.presentation.LightBlue
 
 @Composable
 fun BookListItem(
@@ -12,7 +15,12 @@ fun BookListItem(
     modifier: Modifier = Modifier
 ){
 
-    Surface {
+    Surface(
+        shape = RoundedCornerShape(32.dp),
+        modifier = modifier,
+        onClick = onClick,
+        color = LightBlue.copy(alpha = 0.2f)
+    ) {
 
     }
 

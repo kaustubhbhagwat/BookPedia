6791527f2a78535a5a2273b1b691d4f59320b9a5
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-02-04 20:14:15 +0530

    creating booklist item

---
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/components/BookListItem.kt
@@ -0,0 +1,19 @@
+package org.kb.bookpedia.book.presentation.books_list.components
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.kb.bookpedia.book.domain.Book
+
+@Composable
+fun BookListItem(
+    book: Book,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+
+    Surface {
+
+    }
+
+}

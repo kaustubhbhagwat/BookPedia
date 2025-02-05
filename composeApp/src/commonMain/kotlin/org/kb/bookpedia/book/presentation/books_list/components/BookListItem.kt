0839cf696b09ff7b16befe02982d7aa27f818f5b
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-02-05 19:38:00 +0530

    creating booklist item

--- composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/components/BookListItem.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.CircularProgressIndicator

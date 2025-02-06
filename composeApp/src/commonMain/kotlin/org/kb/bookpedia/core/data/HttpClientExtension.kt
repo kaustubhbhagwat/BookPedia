827a2a5f1b66d0415bfc96d9e8160620eb72b3df
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-02-06 19:41:48 +0530

    started implementing ktor

--- composeApp/src/commonMain/kotlin/org/kb/bookpedia/core/data/HttpClientExtension.kt
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/core/data/HttpClientExtension.kt
@@ -2,9 +2,31 @@ package org.kb.bookpedia.core.data
 
 import io.ktor.client.call.NoTransformationFoundException
 import io.ktor.client.call.body
+import io.ktor.client.network.sockets.SocketTimeoutException
+import io.ktor.client.plugins.ServerResponseException
+import io.ktor.client.request.HttpRequest
 import io.ktor.client.statement.HttpResponse
+import io.ktor.util.network.UnresolvedAddressException
+import kotlinx.coroutines.ensureActive
 import org.kb.bookpedia.core.domain.DataError
 import org.kb.bookpedia.core.domain.Result
+import kotlin.coroutines.coroutineContext
+
+suspend inline fun <reified T> safeCall(
+    execute: () -> HttpResponse
+): Result<T, DataError.Remote> {
+    val response = try {
+        execute()
+    } catch (e: SocketTimeoutException) {
+        return Result.Error(DataError.Remote.REQUEST_TIMEOUT)
+    } catch (e: UnresolvedAddressException) {
+        return Result.Error(DataError.Remote.NO_INTERNET)
+    } catch (e: Exception) {
+        coroutineContext.ensureActive()
+        return Result.Error(DataError.Remote.UNKNOWN)
+    }
+    return responseToResult(response)
+}
 
 suspend inline fun <reified T> responseToResult(
     response: HttpResponse
@@ -18,6 +40,7 @@ suspend inline fun <reified T> responseToResult(
                 Result.Error(DataError.Remote.SERIALIZATION_ERRORS)
             }
         }
+
         408 -> Result.Error(DataError.Remote.REQUEST_TIMEOUT)
         429 -> Result.Error(DataError.Remote.TOO_MANY_REQUEST)
         in 500..599 -> Result.Error(DataError.Remote.SERVER)

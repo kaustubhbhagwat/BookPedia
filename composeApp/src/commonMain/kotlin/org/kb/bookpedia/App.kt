87a86e145387ebe447e8d5cb2130a4635c9e2ed2
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-02-08 20:26:02 +0530

    added http client and engine

--- composeApp/src/commonMain/kotlin/org/kb/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/App.kt
@@ -1,23 +1,29 @@
 package org.kb.bookpedia
 
 import androidx.compose.runtime.*
+import io.ktor.client.engine.HttpClientEngine
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.kb.bookpedia.book.data.network.KtorRemoteBookDataSource
 import org.kb.bookpedia.book.data.repository.DefaultBookRepository
 import org.kb.bookpedia.book.presentation.books_list.BookListScreenRoot
 import org.kb.bookpedia.book.presentation.books_list.BookListViewModel
+import org.kb.bookpedia.core.data.HttpClientFactory
 
 @Composable
 @Preview
-fun App() {
+fun App(engine: HttpClientEngine) {
     BookListScreenRoot(
-        viewModel = remember { BookListViewModel(
-            bookRepository = DefaultBookRepository(
-                remoteBookDataSource = KtorRemoteBookDataSource(
-
+        viewModel = remember {
+            BookListViewModel(
+                bookRepository = DefaultBookRepository(
+                    remoteBookDataSource = KtorRemoteBookDataSource(
+                        httpClient = HttpClientFactory.create(
+                            engine = engine
+                        )
+                    )
                 )
             )
-        ) },
+        },
         onBookClick = {
         }
     )
--- composeApp/src/desktopMain/kotlin/org/kb/bookpedia/main.kt
+++ composeApp/src/desktopMain/kotlin/org/kb/bookpedia/main.kt
@@ -1,13 +1,15 @@
 package org.kb.bookpedia
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import io.ktor.client.engine.okhttp.OkHttp
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "BookPedia",
     ) {
-        App()
+        App(engine = remember { OkHttp.create() })
     }
 }

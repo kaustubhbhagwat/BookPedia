340d0df12103fe62736f0068136994b7e60fa7c0
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-02-13 20:06:20 +0530

    started implementing di with coin

--- composeApp/src/commonMain/kotlin/org/kb/bookpedia/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/di/Modules.kt
@@ -1,13 +1,22 @@
 package org.kb.bookpedia.di
 
-import io.ktor.client.HttpClient
 import org.kb.bookpedia.book.data.network.KtorRemoteBookDataSource
+import org.kb.bookpedia.book.data.network.RemoteBookDataSource
+import org.kb.bookpedia.book.data.repository.DefaultBookRepository
+import org.kb.bookpedia.book.domain.BookRepository
+import org.kb.bookpedia.book.presentation.books_list.BookListViewModel
 import org.kb.bookpedia.core.data.HttpClientFactory
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
+
+expect val platformModule : Module
 
 val sharedModule = module {
     single { HttpClientFactory.create(get()) }
-    single {
-        KtorRemoteBookDataSource(get())
-    }
+    singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>()
+    singleOf(::DefaultBookRepository).bind<BookRepository>()
+    viewModelOf(::BookListViewModel)
 }

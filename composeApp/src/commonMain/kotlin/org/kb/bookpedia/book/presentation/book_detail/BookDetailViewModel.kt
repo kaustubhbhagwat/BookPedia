103b08879f4a430f8bcb8aab04aea9b52baa3c31
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-02-25 13:48:04 +0530

    fetched description

--- composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/book_detail/BookDetailViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/book_detail/BookDetailViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update

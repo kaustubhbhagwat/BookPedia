7633a8f958d46fbae46721955747346dcf9f24d5
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2025-02-26 20:40:21 +0530

    modifier issue fix

--- composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/components/BookList.kt
+++ composeApp/src/commonMain/kotlin/org/kb/bookpedia/book/presentation/books_list/components/BookList.kt
@@ -12,10 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.kb.bookpedia.book.domain.Book
-import org.kb.bookpedia.book.presentation.books_list.BookListScreen
-import org.kb.bookpedia.book.presentation.books_list.BooksListState
 
 @Composable
 fun BookList(
@@ -25,7 +22,7 @@ fun BookList(
     scrollState: LazyListState = rememberLazyListState()
 ) {
     LazyColumn(
-        modifier = Modifier,
+        modifier = modifier,
         state = scrollState,
         verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
